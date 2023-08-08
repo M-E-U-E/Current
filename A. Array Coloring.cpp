1f5b72193d2b0c1637bc3030f8e99a8e9ccb5ca9
Md. Emtiyaj Uddin Emon <137956365+M-E-U-E@users.noreply.github.com>
2023-08-08 20:13:54 +0600

    Add files via upload

---
+++ A. Array Coloring.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+bool cmp(pair<int,int>a,pair<int,int> b)
+{
+    if(a.first > b.first)
+        return true;
+    else if(a.first == b.first and a.second<b.second)
+        return true;
+
+    return false;
+}
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll a[n+5];
+    ll sum = 0;
+    for(int i=0; i<n; i++)
+    {
+        cin >> a[i];
+        sum += a[i];
+    }
+    if(sum%2)
+    {
+        cout << "NO" << endl;
+
+    }
+    else
+    {
+        cout << "YES" << endl;
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+
+    }
+    return 0;
+}
+
