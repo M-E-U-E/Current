2c516b1c6caa7d7e57e25c740d11e2b2a5fc5c4d
Md. Emtiyaj Uddin Emon <137956365+M-E-U-E@users.noreply.github.com>
2023-09-01 22:19:10 +0600

    Add files via upload

---
+++ B. Cat Cycle.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+bool cmp(pair<int,int>a,pair<int,int> b)
+{
+    if(a.first > b.first)
+        return true;
+    else if(a.first == b.first and a.second<b.second)
+        return true;
+
+    return false;
+}
+bool isPrime(int n)
+{
+    // Corner cases
+    if (n <= 1)
+        return false;
+    if (n <= 3)
+        return true;
+
+    // This is checked so that we can skip
+    // middle five numbers in below loop
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+
+    for (int i = 5; i * i <= n; i = i + 6)
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+
+    return true;
+}
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    k--;
+    if(n%2==0)
+    {
+        ll x = k%n;
+        cout << x+1 << endl;
+    }
+    else
+    {
+        ll mid = n/2;
+        ll x = ((k/mid)+k)%n;
+        cout << x+1 << endl;
+    }
+
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+
+    }
+    return 0;
+}
+
+
+
+
