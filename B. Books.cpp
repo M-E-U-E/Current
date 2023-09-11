94df19873e723a8e71a1284b56f06f29295caa37
Md. Emtiyaj Uddin Emon <137956365+M-E-U-E@users.noreply.github.com>
2023-09-12 03:03:10 +0600

    Add files via upload

---
+++ B. Books.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+bool cmp(pair<int,int>a,pair<int,int> b)
+{
+    if(a.first > b.first)
+        return true;
+    else if(a.first == b.first and a.second<b.second)
+        return true;
+
+    return false;
+}
+void solve()
+{
+    ll n, x;
+    cin >> n >> x;
+    ll sum = 0, c=0;
+    ll a[n+5];
+    for(int i=0; i<n; i++)
+    {
+        cin >> a[i];
+
+    }
+//    sort(a,a+n);
+    ll j=0;
+    for(int i=0; i<n; i++)
+    {
+        sum += a[i];
+        if(sum<=x)
+        {
+            c++;
+        }
+        else
+        {
+            sum -= a[j];
+            j++;
+        }
+    }
+    cout << c << endl;
+
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+//    ll t;
+//    cin >> t;
+//    while(t--)
+    {
+        solve();
+
+    }
+    return 0;
+}
+
