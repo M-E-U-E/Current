6811fe39311f09eb6cac31a3380971c81839dd51
Md. Emtiyaj Uddin Emon <137956365+M-E-U-E@users.noreply.github.com>
2023-09-21 04:08:40 +0600

    Add files via upload

---
+++ B. Trouble Sort.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+bool cmp(pair<int,int>a,pair<int,int> b)
+{
+    if(a.first > b.first)
+        return true;
+    else if(a.first == b.first and a.second<b.second)
+        return true;
+
+    return false;
+}
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll a[n+5],b[n+5];
+    for(int i=0; i<n; i++)
+    {
+        cin >> a[i];
+    }
+    ll f=1;
+    for(int i=0; i<n-1; i++)
+    {
+        if(a[i]>a[i+1])
+        {
+            f=0;
+        }
+    }
+    set<ll>s;
+    for(int i=0; i<n; i++)
+    {
+        cin >> b[i];
+        s.insert(b[i]);
+    }
+    if(s.size()==1)
+    {
+        if(f)
+        {
+            cout << "YES" << endl;
+
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    else
+    {
+        cout << "YES" << endl;
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+
+    }
+    return 0;
+}
