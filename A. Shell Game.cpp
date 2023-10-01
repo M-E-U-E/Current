1fa45e9c3ffc6722f1e2b6f0e3cabab5e00ef82e
Md. Emtiyaj Uddin Emon <137956365+M-E-U-E@users.noreply.github.com>
2023-10-02 01:34:05 +0600

    Add files via upload

---
+++ A. Shell Game.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+bool cmp(pair<int,int>a,pair<int,int> b)
+{
+    if(a.first > b.first)
+        return true;
+    else if(a.first == b.first and a.second<b.second)
+        return true;
+
+    return false;
+}
+void solve()
+{
+    ll n, x;
+    cin >> n >> x;
+    n %= 6;
+    while(n--)
+    {
+        if (n&1)
+        {
+            if (x!=0)
+            {
+                x = 3 - x;
+            }
+        }
+        else
+        {
+            if (x != 2)
+            {
+                x = 1 - x;
+            }
+        }
+    }
+    cout << x << endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+//    ll t;
+//    cin >> t;
+//    while(t--)
+    {
+        solve();
+
+    }
+    return 0;
+}
