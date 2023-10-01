0c1bfb1ca2c5830fabd57ae96b799e1bb7971edd
Md. Emtiyaj Uddin Emon <137956365+M-E-U-E@users.noreply.github.com>
2023-10-01 06:27:37 +0600

    Add files via upload

---
+++ A. Jellyfish and Undertale..cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+bool cmp(pair<int,int>a,pair<int,int> b)
+{
+    if(a.first > b.first)
+        return true;
+    else if(a.first == b.first and a.second<b.second)
+        return true;
+
+    return false;
+}
+void solve()
+{
+    ll a, n, b;
+    cin >> a >> b >> n;
+    while(n--)
+    {
+        ll x;
+        cin >> x;
+        b += min(a - 1, x);
+    }
+    cout << b << '\n';
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+
+    }
+    return 0;
+}
