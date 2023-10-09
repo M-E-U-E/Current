2d221a114438f5cfb33d823cec348ec099ac3725
Md. Emtiyaj Uddin Emon <137956365+M-E-U-E@users.noreply.github.com>
2023-10-10 05:26:20 +0600

    Add files via upload

---
+++ A. Modular Exponentiation.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+bool cmp(pair<int,int>a,pair<int,int> b)
+{
+    if(a.first > b.first)
+        return true;
+    else if(a.first == b.first and a.second<b.second)
+        return true;
+
+    return false;
+}
+void solve()
+{
+    ll n, m;
+    cin >> n >> m;
+    if(n>32)
+    {
+        cout << m << endl;
+    }
+    else
+    {
+        ll x = pow(2,n);
+        ll ans = m%x;
+        cout << ans << endl;
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+//    ll t;
+//    cin >> t;
+//    while(t--)
+    {
+        solve();
+
+    }
+    return 0;
+}
+
