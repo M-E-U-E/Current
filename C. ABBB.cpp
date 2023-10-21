2552af3bf566c7b280f46058b54e6143e249ab90
Md. Emtiyaj Uddin Emon <137956365+M-E-U-E@users.noreply.github.com>
2023-10-22 05:59:07 +0600

    Add files via upload

---
+++ C. ABBB.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+bool cmp(pair<int,int>a,pair<int,int> b)
+{
+    if(a.first > b.first)
+        return true;
+    else if(a.first == b.first and a.second<b.second)
+        return true;
+
+    return false;
+}
+void solve()
+{
+    string s;
+    cin >> s;
+    ll a=0, b=0, n=s.size();
+    for(int i=0; i<n; i++)
+    {
+        if(s[i]=='A')
+            {
+                a++;
+
+            }
+        else
+        {
+            if(a)
+            {
+                a--;
+            }
+            else if(b)
+            {
+                b--;
+            }
+            else
+            {
+                b++;
+            }
+        }
+    }
+    cout << a+b << endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+
+    }
+    return 0;
+}
+
