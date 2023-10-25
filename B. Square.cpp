001f680bfb63951061e889d1c97df4c68c2e13c1
Md. Emtiyaj Uddin Emon <137956365+M-E-U-E@users.noreply.github.com>
2023-10-25 18:14:04 +0600

    Add files via upload

---
+++ B. Square.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+bool cmp(pair<int,int>a,pair<int,int> b)
+{
+    if(a.first > b.first)
+        return true;
+    else if(a.first == b.first and a.second<b.second)
+        return true;
+
+    return false;
+}
+void solve()
+{
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    if(a>b) swap(a,b);
+    if(c>d) swap(c,d);
+    if(b==d && a+c==b)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+
+    }
+    return 0;
+}
+
+
