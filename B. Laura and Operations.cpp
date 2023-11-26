2105c40ba9785ec6d06f48c0340b9fd28c2bda31
Md. Emtiyaj Uddin Emon <137956365+M-E-U-E@users.noreply.github.com>
2023-11-27 01:08:17 +0600

    Add files via upload

---
+++ B. Laura and Operations.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+bool cmp(pair<int,int>a,pair<int,int> b)
+{
+    if(a.first > b.first)
+        return true;
+    else if(a.first == b.first and a.second<b.second)
+        return true;
+
+    return false;
+}
+void solve()
+{
+    ll a, b, c;
+    cin >> a >> b >> c;
+    if((abs(b-c)%2==0))
+    {
+        cout << 1 << " ";
+    }
+    else
+    {
+        cout << 0 << " ";
+    }
+    if((abs(a-c)%2==0))
+    {
+        cout << 1 << " ";
+    }
+    else
+    {
+        cout << 0 << " ";
+    }
+    if((abs(a-b)%2==0))
+    {
+        cout << 1 << endl;
+    }
+    else
+    {
+        cout << 0 << endl;
+    }
+
+
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+
+    }
+    return 0;
+}
+
