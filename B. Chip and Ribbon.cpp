f6da5ddec933c55af96a4341758783418f92eb70
Md. Emtiyaj Uddin Emon <137956365+M-E-U-E@users.noreply.github.com>
2023-12-02 19:06:27 +0600

    Add files via upload

---
+++ B. Chip and Ribbon.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+bool cmp(pair<int,int>a,pair<int,int> b)
+{
+    if(a.first > b.first)
+        return true;
+    else if(a.first == b.first and a.second<b.second)
+        return true;
+
+    return false;
+}
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll a[n+5];
+    ll ans = 0;
+    a[0]=0;
+    for(int i=1; i<=n; i++)
+    {
+        cin >> a[i];
+        if(a[i]>a[i-1])
+        {
+            ans += a[i]-a[i-1];
+        }
+    }
+
+    cout << ans-1 << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+
+    }
+    return 0;
+}
+
