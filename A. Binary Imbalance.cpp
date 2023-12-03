7c2085f537be352165c4e9f7c3fc1c363255f3bd
Md. Emtiyaj Uddin Emon <137956365+M-E-U-E@users.noreply.github.com>
2023-12-04 05:33:15 +0600

    Add files via upload

---
+++ A. Binary Imbalance.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+bool cmp(pair<int,int>a,pair<int,int> b)
+{
+    if(a.first > b.first)
+        return true;
+    else if(a.first == b.first and a.second<b.second)
+        return true;
+
+    return false;
+}
+void solve()
+{
+     ll n;
+      cin >> n;
+      string s;
+      cin >> s;
+      for(int i=0; i<n; i++)
+      {
+          if(s[i]=='0')
+          {
+              cout << "YES" << endl;
+              return;
+          }
+      }
+      cout << "NO" << endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+
+    }
+    return 0;
+}
