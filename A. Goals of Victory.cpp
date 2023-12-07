3bab5fee23392f749c5b227ba800081d86b97adb
Md. Emtiyaj Uddin Emon <137956365+M-E-U-E@users.noreply.github.com>
2023-12-08 05:02:49 +0600

    Add files via upload

---
+++ A. Goals of Victory.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+bool cmp(pair<int,int>a,pair<int,int> b)
+{
+    if(a.first > b.first)
+        return true;
+    else if(a.first == b.first and a.second<b.second)
+        return true;
+
+    return false;
+}
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll ans = 0;
+    for(int i=0; i<n-1; i++)
+    {
+        ll x;
+        cin >> x;
+        ans += x;
+    }
+//    cerr << ans << endl;
+    cout << ans*(-1) << endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+
+    }
+    return 0;
+}
