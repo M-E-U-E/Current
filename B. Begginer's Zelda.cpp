5cc0465bbb8cec209ea4b0b6fa8a1da4868342e9
Md. Emtiyaj Uddin Emon <137956365+M-E-U-E@users.noreply.github.com>
2023-12-18 05:14:29 +0600

    Add files via upload

---
+++ B. Begginer's Zelda.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+bool cmp(pair<int,int>a,pair<int,int> b)
+{
+    if(a.first > b.first)
+        return true;
+    else if(a.first == b.first and a.second<b.second)
+        return true;
+
+    return false;
+}
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> degree(n+1,0);
+    for(int i=0; i<n-1; i++)
+    {
+        ll u, v;
+        cin >> u >> v;
+        degree[u]++;
+        degree[v]++;
+    }
+    ll c=0;
+    for(int i=1; i<=n; i++)
+    {
+        if(degree[i]==1)
+        {
+            c++;
+        }
+    }
+    cout << (c+1)/2 << endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+
+    }
+    return 0;
+}
