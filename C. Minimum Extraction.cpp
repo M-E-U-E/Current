8678be727bc156efbeebf6b05d45f9372cbc929e
Md. Emtiyaj Uddin Emon <137956365+M-E-U-E@users.noreply.github.com>
2024-01-23 02:34:29 +0600

    Add files via upload

---
+++ C. Minimum Extraction.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+bool cmp(pair<int,int>a,pair<int,int> b)
+{
+    if(a.first > b.first)
+        return true;
+    else if(a.first == b.first and a.second<b.second)
+        return true;
+
+    return false;
+}
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll a[n+5];
+    for(int i=0; i<n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a, a+n);
+    ll res = a[0];
+    ll sum = 0;
+    for(int i=0; i<n; i++)
+    {
+        a[i]-=sum;
+        sum+=a[i]
+    }
+    for(int i=0; i<n; i++)
+    {
+        res = max(a[i],res);
+    }
+    cout << res << endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+
+    }
+    return 0;
+}
+
+
