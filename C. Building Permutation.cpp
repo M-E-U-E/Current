3780c85c3ab484f901c375b7d33d23ac81b367f7
Md. Emtiyaj Uddin Emon <137956365+M-E-U-E@users.noreply.github.com>
2024-02-17 07:05:02 +0600

    Add files via upload

---
+++ C. Building Permutation.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+bool cmp(pair<int,int>a,pair<int,int> b)
+{
+    if(a.first > b.first)
+        return true;
+    else if(a.first == b.first and a.second<b.second)
+        return true;
+
+    return false;
+}
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll a[n+1];
+    for(int i=0; i<n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a, a+n);
+    ll ans = 0;
+    for(int i=0; i<n; i++)
+    {
+        ans += abs(a[i]-(i+1));
+    }
+    cout << ans << endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+//    ll t;
+//    cin >> t;
+//    while(t--)
+    {
+        solve();
+
+    }
+    return 0;
+}
+
+
+
