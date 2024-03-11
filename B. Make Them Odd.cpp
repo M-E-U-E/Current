e979ff768206e7e6313ee5b181867a5250fe80c1
Md. Emtiyaj Uddin Emon <137956365+M-E-U-E@users.noreply.github.com>
2024-03-12 04:53:43 +0600

    Add files via upload

---
+++ B. Make Them Odd.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+bool cmp(pair<int,int>a,pair<int,int> b)
+{
+    if(a.first > b.first)
+        return true;
+    else if(a.first == b.first and a.second<b.second)
+        return true;
+
+    return false;
+}
+const int N = 1e5+10;
+bitset <N> vis;
+void sieve (void)
+{
+    for (int i = 2; i * i < N; ++i)
+    {
+        if (!vis[i])
+        {
+            for (int j = i * i; j < N; j += i) vis[j] = 1;
+        }
+    }
+}
+void solve()
+{
+    ll n;
+    cin >> n;
+    set<ll> st;
+    while(n--)
+    {
+        ll x;
+        cin >> x;
+        while(x%2==0)
+        {
+            st.insert(x);
+            x/=2;
+        }
+
+    }
+    cout << st.size() << endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+
+    }
+    return 0;
+}
+
