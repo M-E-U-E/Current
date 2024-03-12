c5f681fbc8dc2f1b134ea28c62615b72fe10ef70
Md. Emtiyaj Uddin Emon <137956365+M-E-U-E@users.noreply.github.com>
2024-03-12 16:47:40 +0600

    Add files via upload

---
+++ B. Nirvana.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+bool cmp(pair<int,int>a,pair<int,int> b)
+{
+    if(a.first > b.first)
+        return true;
+    else if(a.first == b.first and a.second<b.second)
+        return true;
+
+    return false;
+}
+const int N = 1e5+10;
+bitset <N> vis;
+void sieve (void)
+{
+    for (int i = 2; i * i < N; ++i)
+    {
+        if (!vis[i])
+        {
+            for (int j = i * i; j < N; j += i) vis[j] = 1;
+        }
+    }
+}
+ll max_pro(ll n)
+{
+    if(n==0)
+    {
+        return 1;
+    }
+    if(n<10)
+    {
+        return n;
+    }
+    return max(max_pro(n/10)*(n%10),max_pro((n/10)-1)*9);
+}
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll ans = max_pro(n);
+    cout << ans << endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+//    ll t;
+//    cin >> t;
+//    while(t--)
+    {
+        solve();
+
+    }
+    return 0;
+}
