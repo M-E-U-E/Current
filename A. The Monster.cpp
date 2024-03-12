81654d48777479fc8971186eb758513e906f4c0e
Md. Emtiyaj Uddin Emon <137956365+M-E-U-E@users.noreply.github.com>
2024-03-13 03:32:57 +0600

    Add files via upload

---
+++ A. The Monster.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+bool cmp(pair<int,int>a,pair<int,int> b)
+{
+    if(a.first > b.first)
+        return true;
+    else if(a.first == b.first and a.second<b.second)
+        return true;
+
+    return false;
+}
+const int N = 1e5+10;
+bitset <N> vis;
+void sieve (void)
+{
+    for (int i = 2; i * i < N; ++i)
+    {
+        if (!vis[i])
+        {
+            for (int j = i * i; j < N; j += i) vis[j] = 1;
+        }
+    }
+}
+void solve()
+{
+    ll a, b, c, d;
+    cin >> a >> b;
+    cin >> c >> d;
+    for(int i=b; i<10000; i+=a)
+    {
+        for(int j=d; j<10000; j+=c)
+        {
+            if(i==j)
+            {
+                cout << i << endl;
+                return;
+            }
+        }
+    }
+    cout << -1 << endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+//    ll t;
+//    cin >> t;
+//    while(t--)
+    {
+        solve();
+
+    }
+    return 0;
+}
+
+
+
