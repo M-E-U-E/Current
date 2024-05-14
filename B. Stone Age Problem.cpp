518678ff3b73ce17cda8f1159e5cf8102104d929
Md. Emtiyaj Uddin Emon <137956365+M-E-U-E@users.noreply.github.com>
2024-05-14 23:47:59 +0600

    Add files via upload

---
+++ B. Stone Age Problem.cpp
@@ -0,0 +1,129 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+bool cmp(pair<int,int>a,pair<int,int> b)
+{
+    if(a.first > b.first)
+        return true;
+    else if(a.first == b.first and a.second<b.second)
+        return true;
+
+    return false;
+}
+//vector<ll>v;
+//void primeFactors(ll n)
+//{
+//    // Print the number of 2s that divide n
+//    while (n % 2 == 0)
+//    {
+//        v.push_back(2);
+//        n = n/2;
+//    }
+//
+//    // n must be odd at this point. So we can skip
+//    // one element (Note i = i +2)
+//    for (ll i = 3; i*i<=n; i = i + 2)
+//    {
+//        // While i divides n, print i and divide n
+//        while (n % i == 0)
+//        {
+//            // cout<<i<<ss;
+//            v.push_back(i);
+//            n = n/i;
+//        }
+//    }
+//
+//    // This condition is to handle the case when n
+//    // is a prime number greater than 2
+//    if (n > 2)
+//        v.push_back(n);
+//}
+const int N = 1e5+10;
+bitset <N> vis;
+void sieve (void)
+{
+    for (int i = 2; i * i < N; ++i)
+    {
+        if (!vis[i])
+        {
+            for (int j = i * i; j < N; j += i) vis[j] = 1;
+        }
+    }
+}
+void solve()
+{
+    ll n, q;
+    cin >> n >> q;
+    ll a[n+4];
+    map<ll,ll> mp1, mp2;
+    ll sum = 0;
+    for(ll i=1; i<=n; i++)
+    {
+        cin >> a[i];
+        sum+=a[i];
+    }
+    ll up = -1;
+    while(q--)
+    {
+        ll x;
+        cin >> x ;
+        if(x==1)
+        {
+            ll p, q;
+            cin >> p >> q;
+            if(up==-1)
+            {
+                if(q>=a[p])
+                {
+                    sum+=(q-a[p]);
+                }
+                else
+                {
+                    sum-=(a[p]-q);
+                }
+                a[p]=q;
+                cout << sum << endl;
+            }
+            else
+            {
+                ll z;
+                if(mp1[p]==0)
+                {
+                    z = up;
+                }
+                else
+                {
+                    z = mp1[p];
+                }
+                if(x>=z){sum+=q-z;}
+                else {sum-=z-q;}
+                cout << sum << endl;
+                mp1[p]=q;
+            }
+        }
+        else
+        {
+            ll x;
+            cin >> x;
+            cout << x*n << endl;
+            sum = x*n;
+            mp1.clear();
+            up = x;
+        }
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+//    ll t;
+//    cin >> t;
+//    while(t--)
+    {
+        solve();
+
+    }
+    return 0;
+}
