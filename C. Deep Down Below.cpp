c166ec76b83303232ebc024b4a228587a1235b8d
Md. Emtiyaj Uddin Emon <137956365+M-E-U-E@users.noreply.github.com>
2024-05-22 09:33:31 +0600

    Add files via upload

---
+++ C. Deep Down Below.cpp
@@ -0,0 +1,118 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+bool cmp(pair<int,int>a,pair<int,int> b)
+{
+    if(a.first > b.first)
+        return true;
+    else if(a.first == b.first and a.second<b.second)
+        return true;
+
+    return false;
+}
+//vector<ll>v;
+//void primeFactors(ll n)
+//{
+//    // Print the number of 2s that divide n
+//    while (n % 2 == 0)
+//    {
+//        v.push_back(2);
+//        n = n/2;
+//    }
+//
+//    // n must be odd at this point. So we can skip
+//    // one element (Note i = i +2)
+//    for (ll i = 3; i*i<=n; i = i + 2)
+//    {
+//        // While i divides n, print i and divide n
+//        while (n % i == 0)
+//        {
+//            // cout<<i<<ss;
+//            v.push_back(i);
+//            n = n/i;
+//        }
+//    }
+//
+//    // This condition is to handle the case when n
+//    // is a prime number greater than 2
+//    if (n > 2)
+//        v.push_back(n);
+//}
+const int N = 1e5+10;
+bitset <N> vis;
+void sieve (void)
+{
+    for (int i = 2; i * i < N; ++i)
+    {
+        if (!vis[i])
+        {
+            for (int j = i * i; j < N; j += i) vis[j] = 1;
+        }
+    }
+}
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<pair<ll,ll>> power(n);
+    for(int i=0; i<n; i++)
+    {
+        ll x;
+        cin >> x;
+        vector<ll> p(x);
+        ll mxp = -1;;
+        for(int j=0; j<x; j++)
+        {
+            cin >> p[j];
+            mxp = max(mxp,p[j]-j+1);
+        }
+        power[i] = {mxp,x};
+    }
+    sort(power.begin(),power.end());
+    ll l = power[0].first;
+    ll r = power[n-1].first;
+    ll ans = 1e12;
+    while(l<=r)
+    {
+        ll mid=(l+r)/2;
+        ll curr=mid;
+        bool ok=1;
+        for(int i=0; i<n; i++)
+        {
+            if(curr>=power[i].first)
+            {
+                curr+=power[i].second;
+            }
+            else
+            {
+                ok=0;
+                break;
+            }
+        }
+        if(ok)
+        {
+            ans=mid;
+            r=mid-1;
+        }
+        else
+        {
+            l=mid+1;
+        }
+    }
+    cout<<ans<<endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+
+    }
+    return 0;
+}
