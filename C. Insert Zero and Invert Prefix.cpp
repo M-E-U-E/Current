1a7a7b3f304af3863db46f33f12857faac3d304e
Md. Emtiyaj Uddin Emon <137956365+M-E-U-E@users.noreply.github.com>
2024-05-31 12:20:56 +0600

    Add files via upload

---
+++ C. Insert Zero and Invert Prefix.cpp
@@ -0,0 +1,116 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+bool cmp(pair<int,int>a,pair<int,int> b)
+{
+    if(a.first > b.first)
+        return true;
+    else if(a.first == b.first and a.second<b.second)
+        return true;
+
+    return false;
+}
+//vector<ll>v;
+//void primeFactors(ll n)
+//{
+//    // Print the number of 2s that divide n
+//    while (n % 2 == 0)
+//    {
+//        v.push_back(2);
+//        n = n/2;
+//    }
+//
+//    // n must be odd at this point. So we can skip
+//    // one element (Note i = i +2)
+//    for (ll i = 3; i*i<=n; i = i + 2)
+//    {
+//        // While i divides n, print i and divide n
+//        while (n % i == 0)
+//        {
+//            // cout<<i<<ss;
+//            v.push_back(i);
+//            n = n/i;
+//        }
+//    }
+//
+//    // This condition is to handle the case when n
+//    // is a prime number greater than 2
+//    if (n > 2)
+//        v.push_back(n);
+//}
+const int N = 1e5+10;
+bitset <N> vis;
+void sieve (void)
+{
+    for (int i = 2; i * i < N; ++i)
+    {
+        if (!vis[i])
+        {
+            for (int j = i * i; j < N; j += i) vis[j] = 1;
+        }
+    }
+}
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for(int i=0; i<n; i++)
+    {
+        cin >> a[i];
+    }
+    ll j = n-1;
+    if(a[j]==1)
+    {
+        cout << "NO" << endl;
+        return;
+    }
+    vector<ll> ans;
+    while(j>=0)
+    {
+        ll noo = 0;
+        ll noz = 0;
+        ll temp_j = j;
+        while(temp_j>=0 && a[temp_j]==0)
+        {
+            noz++;
+            temp_j--;
+        }
+        while(temp_j>=0 && a[temp_j]==1)
+        {
+            noo++;
+            temp_j--;
+        }
+        for(int i=0; i<noz-1; i++)
+        {
+            ans.push_back(0);
+        }
+        for(int i=0; i<noo; i++)
+        {
+            ans.push_back(0);
+        }
+        ans.push_back(noo);
+        j = temp_j;
+    }
+    cout << "YES" << endl;
+    for(auto i:ans)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+
+    }
+    return 0;
+}
