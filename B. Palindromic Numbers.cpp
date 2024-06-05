d0c80ffd5beaf3436c47c622ffba5a11930c1495
Md. Emtiyaj Uddin Emon <137956365+M-E-U-E@users.noreply.github.com>
2024-06-05 20:46:24 +0600

    Add files via upload

---
+++ B. Palindromic Numbers.cpp
@@ -0,0 +1,183 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+bool cmp(pair<int,int>a,pair<int,int> b)
+{
+    if(a.first > b.first)
+        return true;
+    else if(a.first == b.first and a.second<b.second)
+        return true;
+
+    return false;
+}
+//vector<ll>v;
+//void primeFactors(ll n)
+//{
+//    // Print the number of 2s that divide n
+//    while (n % 2 == 0)
+//    {
+//        v.push_back(2);
+//        n = n/2;
+//    }
+//
+//    // n must be odd at this point. So we can skip
+//    // one element (Note i = i +2)
+//    for (ll i = 3; i*i<=n; i = i + 2)
+//    {
+//        // While i divides n, print i and divide n
+//        while (n % i == 0)
+//        {
+//            // cout<<i<<ss;
+//            v.push_back(i);
+//            n = n/i;
+//        }
+//    }
+//
+//    // This condition is to handle the case when n
+//    // is a prime number greater than 2
+//    if (n > 2)
+//        v.push_back(n);
+//}
+const int N = 1e5+10;
+bitset <N> vis;
+void sieve (void)
+{
+    for (int i = 2; i * i < N; ++i)
+    {
+        if (!vis[i])
+        {
+            for (int j = i * i; j < N; j += i) vis[j] = 1;
+        }
+    }
+}
+bool isSmaller(string str1, string str2)
+{
+    // Calculate lengths of both string
+    int n1 = str1.length(), n2 = str2.length();
+
+    if (n1 < n2)
+        return true;
+    if (n2 < n1)
+        return false;
+
+    for (int i = 0; i < n1; i++)
+        if (str1[i] < str2[i])
+            return true;
+        else if (str1[i] > str2[i])
+            return false;
+
+    return false;
+}
+string findDiff(string str1, string str2)
+{
+    // Before proceeding further, make sure str1
+    // is not smaller
+    if (isSmaller(str1, str2))
+        swap(str1, str2);
+
+    // Take an empty string for storing result
+    string str = "";
+
+    // Calculate length of both string
+    int n1 = str1.length(), n2 = str2.length();
+
+    // Reverse both of strings
+    reverse(str1.begin(), str1.end());
+    reverse(str2.begin(), str2.end());
+
+    int carry = 0;
+
+    // Run loop till small string length
+    // and subtract digit of str1 to str2
+    for (int i = 0; i < n2; i++)
+    {
+        // Do school mathematics, compute difference of
+        // current digits
+
+        int sub = ((str1[i] - '0') - (str2[i] - '0') - carry);
+
+        // If subtraction is less then zero
+        // we add then we add 10 into sub and
+        // take carry as 1 for calculating next step
+        if (sub < 0)
+        {
+            sub = sub + 10;
+            carry = 1;
+        }
+        else
+            carry = 0;
+
+        str.push_back(sub + '0');
+    }
+
+    // subtract remaining digits of larger number
+    for (int i = n2; i < n1; i++)
+    {
+        int sub = ((str1[i] - '0') - carry);
+
+        // if the sub value is -ve, then make it positive
+        if (sub < 0)
+        {
+            sub = sub + 10;
+            carry = 1;
+        }
+        else
+            carry = 0;
+
+        str.push_back(sub + '0');
+    }
+
+    // reverse resultant string
+    reverse(str.begin(), str.end());
+
+    return str;
+}
+void solve()
+{
+    ll n;
+    cin >> n;
+    string s;
+    cin >> s;
+    string temp = "";
+    if(s[0]=='9')
+    {
+        for(int i=0; i<n+1; i++)
+        {
+            temp+='1';
+        }
+    }
+    else
+    {
+        for(int i=0; i<n; i++)
+        {
+            temp+='9';
+        }
+    }
+    string final = "";
+    string ans = findDiff(temp,s);
+    ll i=0;
+    while(ans[i]=='0')
+    {
+        i++;
+    }
+    for(; i<ans.size(); i++)
+    {
+        final += ans[i];
+    }
+    cout << final << endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+
+    }
+    return 0;
+}
