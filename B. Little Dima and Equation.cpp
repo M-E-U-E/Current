488a19fb4dcb9f8742abbba432f65e6a36ce02ee
Md. Emtiyaj Uddin Emon <137956365+M-E-U-E@users.noreply.github.com>
2024-06-09 23:27:30 +0600

    Add files via upload

---
+++ B. Little Dima and Equation.cpp
@@ -0,0 +1,98 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+bool cmp(pair<int,int>a,pair<int,int> b)
+{
+    if(a.first > b.first)
+        return true;
+    else if(a.first == b.first and a.second<b.second)
+        return true;
+
+    return false;
+}
+//vector<ll>v;
+//void primeFactors(ll n)
+//{
+//    // Print the number of 2s that divide n
+//    while (n % 2 == 0)
+//    {
+//        v.push_back(2);
+//        n = n/2;
+//    }
+//
+//    // n must be odd at this point. So we can skip
+//    // one element (Note i = i +2)
+//    for (ll i = 3; i*i<=n; i = i + 2)
+//    {
+//        // While i divides n, print i and divide n
+//        while (n % i == 0)
+//        {
+//            // cout<<i<<ss;
+//            v.push_back(i);
+//            n = n/i;
+//        }
+//    }
+//
+//    // This condition is to handle the case when n
+//    // is a prime number greater than 2
+//    if (n > 2)
+//        v.push_back(n);
+//}
+const int N = 1e5+10;
+bitset <N> vis;
+void sieve (void)
+{
+    for (int i = 2; i * i < N; ++i)
+    {
+        if (!vis[i])
+        {
+            for (int j = i * i; j < N; j += i) vis[j] = 1;
+        }
+    }
+}
+void solve()
+{
+    ll a, b, c;
+    cin >> a >> b >> c;
+    vector<ll> v;
+    for (int i = 1; i <= 81; i++)
+    {
+        ll ans = b * pow(i, a) + c;
+        if (ans <= 1e9 && ans > 0)
+        {
+            ll sum_of_digits = 0;
+            ll temp = ans;
+            while (temp > 0)
+            {
+                sum_of_digits += temp % 10;
+                temp /= 10;
+            }
+            if (sum_of_digits == i)
+            {
+                v.push_back(ans);
+            }
+        }
+    }
+
+    cout << v.size() << endl;
+    for (auto x : v)
+    {
+        cout << x << " ";
+    }
+    cout << endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+//    ll t;
+//    cin >> t;
+//    while(t--)
+    {
+        solve();
+
+    }
+    return 0;
+}
