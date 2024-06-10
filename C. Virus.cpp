429c8a5fb40a337854b64c4d6de46e72a940dc24
Md. Emtiyaj Uddin Emon <137956365+M-E-U-E@users.noreply.github.com>
2024-06-10 18:45:46 +0600

    Add files via upload

---
+++ C. Virus.cpp
@@ -0,0 +1,105 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+bool cmp(pair<int,int>a,pair<int,int> b)
+{
+    if(a.first > b.first)
+        return true;
+    else if(a.first == b.first and a.second<b.second)
+        return true;
+
+    return false;
+}
+//vector<ll>v;
+//void primeFactors(ll n)
+//{
+//    // Print the number of 2s that divide n
+//    while (n % 2 == 0)
+//    {
+//        v.push_back(2);
+//        n = n/2;
+//    }
+//
+//    // n must be odd at this point. So we can skip
+//    // one element (Note i = i +2)
+//    for (ll i = 3; i*i<=n; i = i + 2)
+//    {
+//        // While i divides n, print i and divide n
+//        while (n % i == 0)
+//        {
+//            // cout<<i<<ss;
+//            v.push_back(i);
+//            n = n/i;
+//        }
+//    }
+//
+//    // This condition is to handle the case when n
+//    // is a prime number greater than 2
+//    if (n > 2)
+//        v.push_back(n);
+//}
+const int N = 1e5+10;
+bitset <N> vis;
+void sieve (void)
+{
+    for (int i = 2; i * i < N; ++i)
+    {
+        if (!vis[i])
+        {
+            for (int j = i * i; j < N; j += i) vis[j] = 1;
+        }
+    }
+}
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(m);
+    for (int i = 0; i < m; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    vector<int> gaps;
+    for (int i = 0; i < m - 1; i++)
+    {
+        gaps.push_back(a[i + 1] - a[i]-1);
+    }
+    gaps.push_back(n - a.back()-1 + a[0]);
+    int days = 0;
+    int safehouse = 0;
+    sort(gaps.rbegin(), gaps.rend());
+    for (int i = 0; i < gaps.size(); i++)
+    {
+        int currgap = gaps[i];
+        int lost = 2 * days;
+        currgap -= lost;
+        if (currgap <= 0)
+        {
+            break;
+        }
+        safehouse += currgap - 1;
+        if (currgap == 1)
+        {
+            safehouse++;
+        }
+        days+=2;
+    }
+
+    cout << n - safehouse << endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+
+    }
+    return 0;
+}
