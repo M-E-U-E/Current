9f9b576d97a40490c9bede21ffd37dfbf7a35671
Md. Emtiyaj Uddin Emon <137956365+M-E-U-E@users.noreply.github.com>
2024-06-16 19:51:46 +0600

    Add files via upload

---
+++ B. Number Factorization.cpp
@@ -0,0 +1,105 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+bool cmp(pair<int,int>a,pair<int,int> b)
+{
+    if(a.first > b.first)
+        return true;
+    else if(a.first == b.first and a.second<b.second)
+        return true;
+
+    return false;
+}
+//vector<ll>v;
+//void primeFactors(ll n)
+//{
+//    // Print the number of 2s that divide n
+//    while (n % 2 == 0)
+//    {
+//        v.push_back(2);
+//        n = n/2;
+//    }
+//
+//    // n must be odd at this point. So we can skip
+//    // one element (Note i = i +2)
+//    for (ll i = 3; i*i<=n; i = i + 2)
+//    {
+//        // While i divides n, print i and divide n
+//        while (n % i == 0)
+//        {
+//            // cout<<i<<ss;
+//            v.push_back(i);
+//            n = n/i;
+//        }
+//    }
+//
+//    // This condition is to handle the case when n
+//    // is a prime number greater than 2
+//    if (n > 2)
+//        v.push_back(n);
+//}
+const int N = 1e5+10;
+bitset <N> vis;
+void sieve (void)
+{
+    for (int i = 2; i * i < N; ++i)
+    {
+        if (!vis[i])
+        {
+            for (int j = i * i; j < N; j += i) vis[j] = 1;
+        }
+    }
+}
+void solve()
+{
+    ll n, c, sum=0;
+    cin >> n;
+    map<ll,ll>mp;
+    for(ll i = 2 ; i*i <= n ; i++)
+    {
+        c=0;
+        while(n%i == 0)
+        {
+            c++;
+            n/=i;
+            if(!mp.count(c))
+            {
+                mp[c] = i;
+            }
+            else
+            {
+                mp[c]*=i;
+            }
+        }
+    }
+    if(n != 1)
+    {
+        if(!mp.count(1))
+            mp[1] = n;
+        else
+        {
+            mp[1]*=n;
+        }
+    }
+    map<ll,ll>::iterator it;
+    for(it = mp.begin(); it!=mp.end() ; it++)
+    {
+        sum+=it->second;
+    }
+    cout << sum << endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+
+    }
+    return 0;
+}
