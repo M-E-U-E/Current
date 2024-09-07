fc76d8341ef46b4e9232241d8dcc51042fc4b977
Md. Emtiyaj Uddin Emon <137956365+M-E-U-E@users.noreply.github.com>
2024-09-07 22:24:16 +0600

    Add files via upload

---
+++ C. They Are Everywhere.cpp
@@ -0,0 +1,115 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+bool cmp(pair<int,int>a,pair<int,int> b)
+{
+    if(a.first > b.first)
+        return true;
+    else if(a.first == b.first and a.second<b.second)
+        return true;
+
+    return false;
+}
+//vector<ll>v;
+//void primeFactors(ll n)
+//{
+//    // Print the number of 2s that divide n
+//    while (n % 2 == 0)
+//    {
+//        v.push_back(2);
+//        n = n/2;
+//    }
+//
+//    // n must be odd at this point. So we can skip
+//    // one element (Note i = i +2)
+//    for (ll i = 3; i*i<=n; i = i + 2)
+//    {
+//        // While i divides n, print i and divide n
+//        while (n % i == 0)
+//        {
+//            // cout<<i<<ss;
+//            v.push_back(i);
+//            n = n/i;
+//        }
+//    }
+//
+//    // This condition is to handle the case when n
+//    // is a prime number greater than 2
+//    if (n > 2)
+//        v.push_back(n);
+//}
+const int N = 1e5+10;
+bitset <N> vis;
+void sieve (void)
+{
+    for (int i = 2; i * i < N; ++i)
+    {
+        if (!vis[i])
+        {
+            for (int j = i * i; j < N; j += i) vis[j] = 1;
+        }
+    }
+}
+void solve()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    map<char, int> mp;
+    for (int i = 0; i < n; ++i)
+    {
+        mp[s[i]] += 1;
+    }
+    int unq = mp.size();
+    auto check = [&](int mid) -> bool
+    {
+        map<char, int> freq;
+        for (int i = 0; i < mid; ++i)
+        {
+            freq[s[i]] += 1;
+            if (freq.size() == unq) return true;
+        }
+        for (int i = mid, j = 0; i < n; ++i, ++j)
+        {
+            freq[s[i]] += 1;
+            if (freq[s[j]] == 1) freq.erase(s[j]);
+            else freq[s[j]] -= 1;
+            if (freq.size() == unq) return true;
+        }
+
+        return false;
+    };
+    int low = 1, high = n, ans = -1;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (check(mid))
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+
+    cout << ans << endl;
+
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+//    ll t;
+//    cin >> t;
+//    while(t--)
+    {
+        solve();
+
+    }
+    return 0;
+}
